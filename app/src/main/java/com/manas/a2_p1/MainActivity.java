75e106b70a150d96cb424f138c2e578658be3f45
manastyagi <manasvi.tyagi@gmail.com>
2020-10-09 04:21:34 +1300

    API call

--- app/src/main/java/com/manas/a2_p1/MainActivity.java
+++ app/src/main/java/com/manas/a2_p1/MainActivity.java
@@ -2,12 +2,63 @@ package com.manas.a2_p1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    Button getWeatherBtn;
+    TextView resultWeatherText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Lat and Longitude of Auckland
+        final String url = "https://api.openweathermap.org/data/2.5/onecall?lat=-36.8483&lon=174.7625&appid=ab57f83f7f35c8c4355d9fb49b2d0f96";
+        resultWeatherText = findViewById(R.id.weather_result);
+        getWeatherBtn = findViewById(R.id.get_weather_data_btn);
+
+        getWeatherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+                JsonObjectRequest jsonObjectRequest = new
+                        JsonObjectRequest(Request.Method.GET,url,
+                        new JSONObject(),
+                        new com.android.volley.Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String server_data = response.toString();
+                                    JSONObject object = new JSONObject(server_data);
+                                    resultWeatherText.setText(object.getString("current"));
+                                    Log.e("data", response.toString());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("error8", error.toString());
+                    }
+                });
+                queue.add(jsonObjectRequest);
+
+            }
+        });
+
     }
 }
